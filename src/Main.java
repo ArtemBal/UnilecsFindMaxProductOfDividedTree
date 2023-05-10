4e6d28279fdcfabe636bfad4fc4353255335ab53
Artem Balan <a.s.balan96@gmail.com>
2023-05-10 22:45:06 +0300

    Exercise max product of divided tree - https://blog.unilecs.ru/find-max-product-of-tree

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
 public class Main {
+    private static long max = 0;
+
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private static long findTotal(Node root) {
+        if(root == null)
+            return 0;
+        return findTotal(root.left) + findTotal(root.right) + root.value;
+    }
+
+    private static long findMax(Node root, long sum) {
+        if(root == null)
+            return 0;
+        long localSum = findMax(root.left, sum) + findMax(root.right, sum) + root.value;
+        max = Math.max(max, localSum * (sum - localSum));
+        return localSum;
+    }
+
+    private static int findMaxProduct(Node root) {
+        long totalSum = findTotal(root);
+        findMax(root, totalSum);
+        return (int)max;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Node root = new Node(1);
+
+        root.left = new Node(2);
+        root.right = new Node(3);
+
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+
+        root.right.left = new Node(6);
+        System.out.println("110 expected");
+        System.out.println(findMaxProduct(root));
     }
 }
